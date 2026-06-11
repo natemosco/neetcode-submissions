04437641efcdbe47c5a523e5da40a351412c9f64
Nathaniel Mosco <natemosco@gmail.com>
2026-06-10 20:51:23 -0400

    Add: insertionSort - submission-0

---
+++ Data Structures & Algorithms/insertionSort/submission-0.py
@@ -0,0 +1,9 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        
+        return None
