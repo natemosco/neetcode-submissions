d481932f01c3dd18af0087a2b192f7b44e857839
Nathaniel Mosco <natemosco@gmail.com>
2026-06-10 20:52:12 -0400

    Add: insertionSort - submission-2

---
+++ Data Structures & Algorithms/insertionSort/submission-2.cs
@@ -0,0 +1,15 @@
+// Definition for a pair
+// public class Pair {
+//     public int Key;
+//     public string Value;
+//
+//     public Pair(int key, string value) {
+//         Key = key;
+//         Value = value;
+//     }
+// }
+public class Solution {
+    public List<List<Pair>> InsertionSort(List<Pair> pairs) {
+
+    }
+}
